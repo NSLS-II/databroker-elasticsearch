34eea8939925682a5f6bcff7e1182c9407a98ddd
christopher <cjwright4242@gmail.com>
2018-10-21 17:24:01 -0400

    use `indices` attr

--- db_es/tests/conftest.py
+++ db_es/tests/conftest.py
@@ -5,7 +5,7 @@ from elasticsearch import Elasticsearch
 @pytest.fixture(scope='module')
 def es(host="127.0.0.1"):
     e = Elasticsearch(hosts=[host])
-    ic = e.IndicesClient()
+    ic = e.indices
     ic.create('xpd')
     ic.create('iss')
     return e
