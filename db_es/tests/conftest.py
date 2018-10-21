243dca3d69cb889ef2cd4641621f2826d3d7bac8
christopher <cjwright4242@gmail.com>
2018-10-21 17:06:29 -0400

    mark as fixture

--- db_es/tests/conftest.py
+++ db_es/tests/conftest.py
@@ -1,6 +1,8 @@
+import pytest
 from elasticsearch import Elasticsearch
 
 
+@pytest.fixture(scope='module')
 def es(host="127.0.0.1"):
     e = Elasticsearch(hosts=[host])
     # XXX: maybe need to make index here?
