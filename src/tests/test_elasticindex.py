ab947fc0eda2732afefddbbe9f9323e5e96a2a36
Pavol Juhas <pavol.juhas@gmail.com>
2018-11-18 22:29:31 -0500

    TST: flesh out tests for ElasticIndex
    - use separate temporary ES index per each test
    - mark up failing tests

--- src/tests/test_elasticindex.py
+++ src/tests/test_elasticindex.py
@@ -28,11 +28,51 @@ def test___init__(es):
 
 
 def test_reset(es):
-    ei = ElasticIndex(es, 'dbes-test-foo')
+    ei = ElasticIndex(es, 'dbes-test-reset')
     bqall = {"query": {"match_all": {}}}
     with pytest.raises(NotFoundError):
         es.search(index=ei.index, body=bqall)
     ei.reset()
+    es.indices.refresh()
     res = es.search(index=ei.index, body=bqall)
     assert res['hits']['total'] == 0
+    return
+
+
+@pytest.mark.xfail
+def test_add(es):
+    ei = ElasticIndex(es, 'dbes-test-add')
+    ei.reset()
+    doc1 = {"_id": 1, "fruit": "apple"}
+    assert ei.add(doc1) == 1
+    # check ElasticIndex.criteria
+    doc2 = {"_id": 2, "fruit": "banana"}
+    ei.criteria = lambda e: e['fruit'] != "banana"
+    assert ei.add(doc2) == 0
+    # check ElasticIndex.mapper
+    ei.mapper = lambda e: dict(e, fruit=e['fruit'].upper())
+    doc3 = {"_id": 3, "fruit": "cantaloupe"}
+    assert ei.add(doc3) == 1
+    bqall = {"query": {"match_all": {}}}
+    es.indices.refresh()
+    res = es.search(index=ei.index, body=bqall)
+    assert res['hits']['total'] == 2
+    src = es.get(index=ei.index, doc_type=ei.doc_type, id=3)['_source']
+    assert src['fruit'] == 'CANTALOUPE'
+    return
+
+
+@pytest.mark.xfail
+def test_bulk(es):
+    ei = ElasticIndex(es, 'dbes-test-bulk')
+    ei.reset()
+    docs = [
+        {"_id": 1, "fruit": "apple"},
+        {"_id": 2, "fruit": "banana"},
+        {"_id": 3, "fruit": "cantaloupe"},
+    ]
+    assert ei.bulk(docs) == 3
+    es.indices.refresh()
+    cnt = int(es.cat.count(ei.index, h='count'))
+    assert cnt == 3
     return
