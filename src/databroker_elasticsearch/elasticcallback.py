b3d898835c3a75b3d8551a0fc87db1f18b9bd08c
Pavol Juhas <pavol.juhas@gmail.com>
2018-12-03 18:30:54 -0500

    DOC: fix docstrings for ElasticCallback class

--- src/databroker_elasticsearch/elasticcallback.py
+++ src/databroker_elasticsearch/elasticcallback.py
@@ -9,68 +9,21 @@ from databroker_elasticsearch.elasticindex import ElasticIndex
 
 
 class ElasticCallback(CallbackBase):
-    "FIXME" + 0 * \
-    """Callback for inserting start metadata into an ElasticSearch instance
+    """
+    Callback for inserting start metadata into an `Elasticsearch` instance.
 
-    Examples
-    --------
-    Run imports
-    >>> from elasticsearch import Elasticsearch
-    >>> from databroker_elasticsearch.callback import ElasticInsert
-    >>> from databroker_elasticsearch.callback import noconversion, toisoformat
+    This wraps `ElasticIndex` adapter for adding translated documents to
+    a given Elasticsearch index.
 
-    Assuming that the Elasticsearch instance is running on local host
-    >>> es = Elasticsearch(hosts=['127.0.0.1'])
-
-    Create a list of tuples which is the `('data_name',
-    'elasticsearch_name', converter)` The `data_name` is the name in the
-    data and `elasticsearch_name` is the name in elasticsearch. The
-    converter is a function which converts the data from the type from the
-    experiment to a type that elasticsearch can understand, most times
-    `noconversion` will work
-    >>> docmap = [('bt_piLast', 'pi', noconversion), ('cycle', 'cycle', int)]
-
-    Create an instance of ElasticInsert
-    >>> ei = ElasticInsert(es, esindex='xpd', docmap=docmap, beamline='xpd')
-
-    Optionally an additional criteria function can be given. When provided
-    the function is run on all the incoming data. If the function returns
-    True the data will be copied to Elasticsearch, otherwise it will not.
-    This can be used to prevent propritary data from being exposed.
-    >>> ei = ElasticInsert(es, esindex='xpd', docmap=docmap, beamline='xpd',
-    >>>                    criteria=lambda x: 'CJ' in x['bt_piLast'])
-
-    Once the callback has been setup it can be subscribed to the RunEngine so
-    that all subsequent data is added to elasticsearch
-    >>> RE.subscribe(ei)
-
-    Or data from the databroker can be sent through, for initializing
-    elasticsearch
-    >>> for hdr in db():
-    ...     for name, doc in hdr.documents():
-    ...         ei(name, doc)
+    Attributes
+    ----------
+    esindex : ElasticIndex
+        The ElasticIndex object for converting documents and inserting
+        them to Elasticsearch.
     """
 
-    def __init__(self, esindex: ElasticIndex):
-        "FIXME" + 0 * \
-        """Init callback
 
-        Parameters
-        ----------
-        es : Elasticsearch instance
-            The Elasticsearch client to push entries to
-        esindex : str
-            The index to use in Elasticsearch
-        docmap : list
-            A map between keys in the start document and keys in the ES
-            instance, with conversions
-        beamline : str
-            Name of beamline
-        criteria : callable, optional
-            Callable which is run on all the start document, if the return is
-            truthy the document is sent to ES else, it is not added. Defaults
-            to True for all documents
-        """
+    def __init__(self, esindex: ElasticIndex):
         self.esindex = esindex
         return
 
@@ -96,14 +49,24 @@ class ElasticCallback(CallbackBase):
         return rv
 
 
-    def start(self, doc):
-        # FIXME should we cache and ingest in stop() to avoid canceled runs?
-        self.esindex.ingest(doc)
-        return
-
-
     def rebuild(self, db):
+        """Purge ES index and re-export all start documents from databroker.
+
+        Parameters
+        ----------
+        db : databroker.Broker
+            The Broker instance that has all prior "start" documents.
+        """
         startdocs = (hdr.start for hdr in db())
         self.esindex.reset()
         cnt = self.esindex.devour(startdocs)
         return cnt
+
+    # override CallbackBase function
+
+    def start(self, doc):
+        """Handle "start" document in bluesky run engine callback.
+        """
+        # FIXME should we cache and ingest in stop() to avoid canceled runs?
+        self.esindex.ingest(doc)
+        return
