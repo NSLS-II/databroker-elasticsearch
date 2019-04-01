c3a1244be2eba8bc8cedc14f9e1f5e7a42addef2
Pavol Juhas <pavol.juhas@gmail.com>
2019-04-01 12:09:37 -0400

    ENH: add BrokerSearch class for Headers retrieval

---
+++ src/databroker_elasticsearch/brokersearch.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+"""\
+Retrieve databroker Headers correspoding to an Elasticsearch query.
+"""
+
+from databroker._core import Results
+
+
+class BrokerSearch:
+    """
+    Callable to retrieve databroker `Header` objects from ES query.
+
+    Attributes
+    ----------
+    db : databroker.Broker
+        The databroker instance for looking up `Header` objects.
+    esindex : ElasticIndex
+        The ElasticIndex instance to use for ES queries.
+    """
+
+    def __init__(self, db, esindex):
+        self.db = db
+        self.esindex = esindex
+        return
+
+
+    def __call__(self, q, **kwargs):
+        """
+        Execute search using Lucene query string syntax.
+
+        Parameters
+        ----------
+        q : str
+            The string search query in Lucene syntax.
+        kwargs : misc, optional
+            Extra arguments passed to the `Elasticsearch.search` function.
+
+        Returns
+        -------
+        databroker.Results
+            Iterable object encapsulating the matching databroker Headers.
+        """
+        from elasticsearch.helpers import scan
+        es = self.esindex.es
+        index = self.esindex.index
+        doc_type = self.esindex.doc_type
+        query = {'q': q}
+        gscan = scan(es, query, index=index, doc_type=doc_type,
+                     _source=['uid'], **kwargs)
+        gstartstop = ((e['_source']['uid'], None) for e in gscan)
+        rv = Results(gstartstop, self.db, data_key=None)
+        return rv
