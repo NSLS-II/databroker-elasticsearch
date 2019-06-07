173012b325da742a56f277aa579247d46415258e
Pavol Juhas <pavol.juhas@gmail.com>
2019-06-07 17:08:15 -0400

    REL: add license and GitHub URL to setup.py

--- setup.py
+++ setup.py
@@ -16,14 +16,17 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    zip_safe=False,
+    include_package_data=True,
     description='Databroker Elasticsearch bridge',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    zip_safe=False,
-    include_package_data=True,
+    license='BSD (3-clause)',
+    url='https://github.com/NSLS-II/databroker-elasticsearch',
     classifiers = [
         # List of possible values at
         # https://pypi.python.org/pypi?:action=list_classifiers
+        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
